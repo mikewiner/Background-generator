3de3806facf7dcf735e39513ef6c05f5e7855fe5
mikewiner <winer.mike@gmail.com>
2020-07-09 19:32:05 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.querySelector("button");
+
+changeBackground();
+
+function randomIt(){
+	color1.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	color2.value = "#" + Math.floor(Math.random()*16777215).toString(16); 
+	changeBackground();
+}
+
+function changeBackground (){
+	body.style.background = 
+	"linear-gradient(to right, "
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+")";
+
+	css.textContent = body.style.background;
+}
+
+
+
+color1.addEventListener("input", changeBackground);
+color2.addEventListener("input", changeBackground);
+button.addEventListener("click", randomIt);
+
